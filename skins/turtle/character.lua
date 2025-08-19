da22239cec18979c17e392cd2aa8cbaf5f0ecc42
Eduard Hendriksen <eduard@complect-ed.nl>
2025-08-19 14:37:39 +0200

    fixed Turtle WoW title dropdown;

---
+++ skins/turtle/character.lua
@@ -0,0 +1,18 @@
+pfUI:RegisterSkin("Character Frame Turtle", "vanilla", function()
+  -- skin is loaded after pfUI is booted - so check if it is disabled ourselves
+  if (pfUI_config["disabled"] and
+    pfUI_config["disabled"]["skin_Inspect Turtle"]  == "1") then
+  return
+  end
+
+  -- skin title dropdown menu
+  -- taken from: https://github.com/doorknob6/pfUI-turtle/blob/master/skins/turtle/character.lua
+  if PaperDollFrameTitlesDropdown and pfUI.skin["Character"] and pfUI_config["disabled"]["skin_Character"] ~= "1" then
+    CharacterLevelText:SetPoint("TOP", CharacterNameText, "BOTTOM", 0, -2)
+    SkinDropDown(PaperDollFrameTitlesDropdown)
+    PaperDollFrameTitlesDropdown:SetPoint("TOP", CharacterGuildText, "BOTTOM", 0, -2)
+    CharacterTitleText:SetPoint("LEFT", PaperDollFrameTitlesDropdown.backdrop, "LEFT", 6, 2)
+    CharacterResistanceFrame:SetPoint("TOP", PaperDollFrameTitlesDropdown, "BOTTOM", 0, 0)
+  end
+
+end)
