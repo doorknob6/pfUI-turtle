348c7f2f6ac87f974e2b6a071a110896468f0cab
doorknob6 <joopkjongste@gmail.com>
2025-08-21 16:55:00 +0200

    updated druid moonkin autoshift module;

--- modules/autoshift.lua
+++ modules/autoshift.lua
@@ -1,22 +1,25 @@
-local moonkin_scan = CreateFrame("Frame")
-moonkin_scan:RegisterEvent("PLAYER_ENTERING_WORLD")
-moonkin_scan:RegisterEvent("UNIT_NAME_UPDATE")
-moonkin_scan:RegisterEvent("PLAYER_TALENT_UPDATE")
-moonkin_scan:RegisterEvent("CHARACTER_POINTS_CHANGED")
-moonkin_scan:SetScript("OnEvent", function()
-local _, class = UnitClass("player")
-if class == "DRUID" then
-  for i = table.getn(pfUI.autoshift.shapeshifts), 1, -1 do
-	if pfUI.autoshift.shapeshifts[i] == "spell_nature_forceofnature" then
-	  table.remove(pfUI.autoshift.shapeshifts, i)
-	end
+pfUI:RegisterModule("Turtle WoW druid moonkin autoshift", "vanilla", function ()
+  if (pfUI_config["disabled"] and pfUI_config["disabled"]["Turtle WoW druid moonkin autoshift"]  == "1") then
+    return
   end
-  local _,_,_,_,moonkin = GetTalentInfo(1,15)
-  if moonkin == 1 then
-	table.insert(pfUI.autoshift.shapeshifts, "spell_nature_forceofnature")
-	moonkin_scan:UnregisterAllEvents()
-  end
-else
-  moonkin_scan:UnregisterAllEvents()
-end
+
+  local _, myclass = UnitClass("player")
+  if myclass ~= "DRUID" then return end
+
+  local moonkinScan = CreateFrame("Frame")
+  moonkinScan:RegisterEvent("PLAYER_ENTERING_WORLD")
+  moonkinScan:RegisterEvent("UNIT_NAME_UPDATE")
+  moonkinScan:RegisterEvent("PLAYER_TALENT_UPDATE")
+  moonkinScan:RegisterEvent("CHARACTER_POINTS_CHANGED")
+  moonkinScan:SetScript("OnEvent", function()
+    for i = table.getn(pfUI.autoshift.shapeshifts), 1, -1 do
+    if pfUI.autoshift.shapeshifts[i] == "spell_nature_forceofnature" then
+      table.remove(pfUI.autoshift.shapeshifts, i)
+    end
+    end
+    local _,_,_,_,moonkin = GetTalentInfo(1,15)
+    if moonkin == 1 then
+      table.insert(pfUI.autoshift.shapeshifts, "spell_nature_forceofnature")
+    end
+  end)
 end)
